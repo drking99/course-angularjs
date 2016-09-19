82939339b2bb509076cc97ec9c9e145da1dae446
Dinesh Komerelley <Nirvana@Nirvana.local>
2016-09-19 00:24:15 -0400

    module1-solution added border color and message color logic

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -12,10 +12,13 @@
         var arrDishesEntered = lstOfDishes.length === 0 ? "" : lstOfDishes.split(',');
         if(arrDishesEntered.length > 0 && arrDishesEntered.length <= 3){
           $scope.message = "Enjoy!";
+          $scope.msgColor = "Green";
         }else if (arrDishesEntered.length > 3) {
           $scope.message = "Too much!" ;
+          $scope.msgColor = "Green";
         }else{
           $scope.message = "Please enter data first"
+          $scope.msgColor = "Red";
         }
       }
 
